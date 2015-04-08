990fc91274e7a65a63c705c8625fcd6920de28e3
Justin Loew <jloloew@gmail.com>
2015-04-08 17:34:46 -0500

    Initial Commit

---
+++ TodayIPAddress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayIPAddress
+//
+//  Created by Justin Loew on 4/8/15.
+//  Copyright (c) 2015 Lustin' Joew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodayIPAddress/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TodayIPAddress
+//
+//  Created by Justin Loew on 4/8/15.
+//  Copyright (c) 2015 Lustin' Joew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
